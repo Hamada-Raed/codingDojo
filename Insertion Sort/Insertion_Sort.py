418a2f59ed2d499f7fa4786321b426a5c19ea705
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-05-09 00:05:21 +0300

    env

--- Insertion Sort/Insertion_Sort.py
+++ Insertion Sort/Insertion_Sort.py
@@ -2,12 +2,12 @@ def InsertionSort(arr):
 
     for i in range(1, len(arr)): # i = 1
 
-        temp = arr[i]
-        j = i-1
-        while j >= 0 and temp < arr[j]:
-            arr[j+1] = arr[j]
-            j -= 1
-        arr[j+1] = temp
+        temp = arr[i] #arr[1]
+        j = i-1  #j=0 
+        while j >= 0 and temp < arr[j]: # temp < arr[0]
+            arr[j+1] = arr[j] #arr[1] = arr[0]
+            j -= 1 #=> j=-1
+        arr[j+1] = temp #=> arr[1] = arr[1]
     return arr
 
 arr = [10, 5, 13, 8, 2]

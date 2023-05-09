3276ec2484fabe4f33d055b279bee120b34c7679
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-05-09 15:45:18 +0300

    env

--- project_env/bin/activate.fish
+++ project_env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/HamadaRaed1999/Desktop/project_env"
+set -gx VIRTUAL_ENV "/Users/HamadaRaed1999/Desktop/codingCojo/project_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

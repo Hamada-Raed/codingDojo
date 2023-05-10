1d843aa1d5d6ea966245eb2db0a2c8503802d73d
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-05-11 02:13:02 +0300

    User

--- chaining_methods/chanining.py
+++ chaining_methods/chanining.py
@@ -1,4 +1,5 @@
 
+
 class User:		# here's what we have so far
     def __init__(self, name, email, account_balance):
         self.name = name
@@ -7,24 +8,20 @@ class User:		# here's what we have so far
 
     def deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def withdrawal(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
-
         print(
             f"The username is {self.name} and the balance is ${self.account_balance}")
-
-    def transfer(from_account, to_account, account_balance):
-        from_account.withdrawal(account_balance)
-        to_account.deposit(account_balance)
+        return self
 
 
 Hamada = User("Hamada Raed", "Hamada@gmail.com", 1000)
 Mohammad = User("Mohammad Tamer ", "Mohammad@gmail.com", 1230)
 Nehal = User("Nehal Omer ", "Nehal@gmail.com", 4903)
 
-
-Hamada.display_user_balance()
-Mohammad.display_user_balance()
+Hamada.deposit(100).withdrawal(400).deposit(30).display_user_balance()

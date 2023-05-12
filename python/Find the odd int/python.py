eb9a78244a79a82b33d35baa3f5ebb025d8d9e7a
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-05-12 17:15:16 +0300

    commit

--- python/Find the odd int/python.py
+++ python/Find the odd int/python.py
@@ -2,11 +2,8 @@
 # There will always be only one integer that appears an odd number of times.
 
 def counter_of_odd(arr):
-    count = arr[0] 
-    
-    for i in range(len(arr)):  
-        if arr[0] == arr[i]: 
-            arr[i] = count
-    return count
 
-print(counter_of_odd([2, 2, 3, 1, 1,2,2,2,2]))
+    pass
+
+
+print(counter_of_odd([2,2,2,1,3,3,3,3,3]))

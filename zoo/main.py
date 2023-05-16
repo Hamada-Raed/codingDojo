64c0c6165469552f72585ab65f000698b43f5a15
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-05-16 22:03:57 +0300

    new commit

--- zoo/main.py
+++ zoo/main.py
@@ -10,7 +10,7 @@ class Animal:
     def __init__(self, name, age, healty_level, happiness_level):
         self.name = name
         self.age = age
-        self.healty_level = happiness_level
+        self.healty_level = healty_level
         self.happiness_level = happiness_level
 
     def feed(self):
@@ -19,12 +19,11 @@ class Animal:
 
     def display_info(self):
         print(
-            f"Name: {self.name}\n healty level: {self.healty_level}\n happiness level: {self.happiness_level}")
+            f"Name: {self.name}\n healty level:{self.healty_level}\n happiness level:{self.happiness_level}")
 
 
 class Zoo:
-    def __init__(self, zoo_name ,name, age, healty_level, happiness_level):
-        super().__init__(name, age, healty_level, happiness_level)
+    def __init__(self, zoo_name):
         self.zoo_name = zoo_name
         self.animals = []
 
@@ -33,11 +32,41 @@ class Zoo:
         self.animals.append(new_animal)
 
     def display_animal(self):
-        super().display_info()
-        for animal in  self.animals: 
-            print (animal)
-
-    
+        for animal in self.animals:
+            print(animal)
 
 
+class Lion(Animal):
+    def __init__(self, kind, healty_level=1, happiness_level=2):
+        self.kind = kind
+        super().__init__(healty_level, happiness_level)
 
+    def feed(self):
+        self.healty_level += 5
+        self.happiness_level += 4
+
+
+class Tiger(Animal):
+    def __init__(self, kind, healty_level=1, happiness_level=2):
+        self.kind = kind
+        super().__init__(healty_level, happiness_level)
+
+    def feed(self):
+        self.healty_level += 9
+        self.happiness_level += 3
+
+
+class Bear(Animal):
+    def __init__(self, kind, healty_level=1, happiness_level=2):
+        self.kind = kind
+        super().__init__(healty_level, happiness_level)
+
+    def feed(self):
+        self.healty_level += 3
+        self.happiness_level += 8
+
+
+Lion1 = Lion("Dog", "Dog", 1, 1, 1)
+Lion1.display_info()
+Lion1.feed()
+Lion1.display_info()

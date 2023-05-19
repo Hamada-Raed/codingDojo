83187a247eddb0f79ba84fd576d61294d4692e41
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-05-19 22:45:13 +0300

    new commit

---
+++ Flask/flask/chess/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template 
+app = Flask(__name__) 
+
+@app.route('/')
+def staticChess(): 
+    return render_template('index.html')
+
+@app.route('/<x>')
+def dynamicChess_in_x(x):
+    input_x = int(x)
+    return render_template('index1.html' , input_x = input_x)
+
+@app.route('/<x>/<y>')
+def dynamicChess_in_x_y(x,y):
+    input_x = int(x)
+    input_y = int(y)
+    return render_template('index2.html', input_x = input_x, input_y = input_y)
+
+@app.route('/<x>/<y>/<color>/<color1>')
+def dynamicChess_in_x_y_color(x,y,color,color1):
+    input_x = int(x)
+    input_y = int(y)
+    input_color = color
+    input_color1 = color1
+    return render_template('index3.html', input_x=input_x, input_y=input_y, input_color1=input_color1, input_color= input_color)
+
+if __name__=="__main__":
+    app.run(debug=True)

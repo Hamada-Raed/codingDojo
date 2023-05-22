a65080e68266b4e58d2a418196b0d1200c978bfa
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-05-22 16:19:30 +0300

    commit

--- Flask/flask/Dojo Fruit Store/main.py
+++ Flask/flask/Dojo Fruit Store/main.py
@@ -1,8 +1,9 @@
 from flask import Flask , render_template, request, redirect
+from datetime import datetime
 app = Flask(__name__) 
 
 @app.route('/')
-def display_main_page():
+def display_main_page(): 
     return render_template('index.html')
 
 @app.route('/checkout', methods=['post'])
@@ -14,7 +15,9 @@ def result():
     Kiwi_from_form = request.form['Kiwi']
     name_form_form = request.form['name']
     id_form_form = request.form['id']
-    return render_template ('result.html', Orange_on_form=Orange_from_form, Apple_on_form= Apple_from_form, Kiwi_on_form=Kiwi_from_form, name_on_form=name_form_form, id_on_form=id_form_form   )
+    date = str(datetime.now())
+    total = (int(request.form['Orange']) + int(request.form['Apple']) + int(request.form['Kiwi']))
+    return render_template ('result.html', Orange_on_form=Orange_from_form, Apple_on_form= Apple_from_form, Kiwi_on_form=Kiwi_from_form, name_on_form=name_form_form, id_on_form=id_form_form,date=date,Total=total)
 
 
 if __name__ == '__main__':

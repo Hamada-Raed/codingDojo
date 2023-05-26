173655dbb6574556bbcd9e0dc08c6b5ab5ad6deb
Hamada <HamadaRaed1999@SHARAFCOMPUTERs-MacBook-Air.local>
2023-05-26 13:51:46 +0300

    new commit

--- Flask/flask/great_num_Game/main.py
+++ Flask/flask/great_num_Game/main.py
@@ -9,8 +9,14 @@ def index():
     if 'message' not in session:
         session["message"]=""
     if 'number' not in session:
-        session['number']=random.randrange(1,101)
-    return render_template("index.html", message=session['message'] )
+        session['number']=random.randint(1,100)
+    print (session['number'])
+    if 'color' not in session:
+        session['color'] = ''
+    if 'counter' not in session: 
+        session['counter'] = 15
+   
+    return render_template("index.html", message=session['message'], color =session['color'],counter= session['counter'])
 
 @app.route('/guess', methods=['POST'])
 def guess():
@@ -18,15 +24,26 @@ def guess():
     guess=int(request.form['number'])
     if guess== session['number']:
         session['message']= "you win!"
+        session['color'] = 'green'
     if guess > session['number']:
         session['message']= 'Too high guess lower'
+        session['color'] = 'red'
     elif guess< session['number']:
         session['message']= 'Too low guess higer'
+        session['color'] = 'red'
+    
+    session['counter'] -= 1 
+    if session['counter'] <= 0:
+        session['message']= "You lose"
+
     return redirect('/')
+
+
 @app.route('/reset')
 def reset():
-    session['number']
-    session.pop("number")
-    session.pop("message")
+    session.clear()
     return redirect('/')
-app.run(debug=True)
+
+if __name__ == "__main__": 
+    app.run(debug = True)
+

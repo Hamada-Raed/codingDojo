7aec3da297323a5d38332d1f67e3362cbac96ecb
Hamada <HamadaRaed1999@SHARAFCOMPUTERs-MacBook-Air.local>
2023-05-27 00:22:18 +0300

    new commit

---
+++ Flask/flask/Ninjs_gold/server.py
@@ -0,0 +1,41 @@
+from flask import Flask , render_template, request, redirect, session
+import random
+import time
+app = Flask(__name__) 
+app.secret_key='secret'
+
+@app.route('/')
+def start():
+    if 'gold' not in session: 
+        session['gold'] = 0 
+    return render_template('index.html', gold=session['gold'])
+
+@app.route('/collect_money', methods=['POST'])
+def collect_money():
+    
+    method=request.form['building'] 
+    if method == 'farm':
+        amount = random.randint(10,20)
+        text = f"earned {amount} golds from the Farm!"
+        session['gold']+= amount
+    elif method == 'cave': 
+        amount = random.randint(5,10)
+        text = f"earned {amount} golds from the Cave!"
+        session['gold']+= amount
+    elif method == 'house': 
+        amount = random.randint(2,5)
+        text = f"earned {amount} golds from the House!"
+        session['gold']+= amount
+    elif method == 'casino': 
+        amount = random.ransdint(-50,50)
+        text = f"earned {amount} golds from the Casino!"
+        session['gold']+= amount 
+
+    return redirect('/')
+
+
+
+
+if __name__ == "__main__": 
+    app.run(debug = True)
+
